2c89241f7f1bebd910bd19481dd8ab7cd6b99761
quartz-06 <seokquartz06@gmail.com>
2025-12-19 20:55:26 +0900

    12/19/20:55

---
+++ Assets/Scripts/Scy/Main_MenuController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Main_MenuController : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
